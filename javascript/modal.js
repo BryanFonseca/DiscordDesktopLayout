8139a44d22fda8bfc9cc7ca364726ee0567a0da4
Bryan Fonseca <isaac1805@hotmail.com>
2021-10-23 01:21:29 -0500

    Add modal closeness (?

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,9 +1,13 @@
 const buttonClose = document.querySelector('.modal-close');
+const modalBackdrop = document.querySelector('.modal-backdrop');
 const modal = document.querySelector('.modal');
 
-buttonClose.addEventListener('click', (e) => {
+const hideModal = () => {
   modal.style.opacity = '0';
   setTimeout(() => {
     modal.classList.add('invisible');
   }, 500);
-});
+}
+
+modalBackdrop.addEventListener('click', () => hideModal());
+buttonClose.addEventListener('click', () => hideModal());
