b2c5829a1cc739a3092b4710f8e30ebb1bf79b11
Bryan Fonseca <isaac1805@hotmail.com>
2021-10-23 23:52:00 -0500

    Add message sending and "refactoring"

---
+++ javascript/sendMessage.js
@@ -0,0 +1,47 @@
+const messageInput = document.querySelector('.input-message input');
+const sendButton = document.querySelector('.send-message-button');
+const chatContainer = document.querySelector('.chat');
+
+const scrollToBottom = () => {
+  chatContainer.scroll({
+    top: chatContainer.scrollHeight, 
+    left: 0,
+    behavior: 'smooth',
+  });
+}
+
+scrollToBottom();
+
+const sendMessage = () => {
+  const messageText = messageInput.value;
+  const sendTime = new Date().getHours() + ':' + new Date().getMinutes();
+  if(messageText)
+  {
+    const message = `
+      <div class="message">
+	<div class="icon-circle">
+	  <img class="friend-img" src="avatars/me.jpg" alt="friend picture">
+	</div>
+	<div class="message-data">
+	  <div class="message-inner-data">
+	    <span class="message-sender">Bryan</span>
+	    <span class="message-date">today ${sendTime}</span>
+	  </div>
+	  <span class="message-text">${messageText}</span>
+	</div>
+      </div>
+    `;
+    chatContainer.insertAdjacentHTML('beforeend', message);
+    messageInput.value = '';
+    scrollToBottom();
+  }
+};
+
+sendButton.addEventListener('click', () => {
+  sendMessage();
+});
+
+messageInput.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter')
+    sendMessage();
+});
